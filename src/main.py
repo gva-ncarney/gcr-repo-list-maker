9d958aba20336b0f3c8149f8fee43f220f784fe6
gva-ncarney <57491852+gva-ncarney@users.noreply.github.com>
2021-05-06 08:02:36 +0100

    ty

--- src/main.py
+++ src/main.py
@@ -1,33 +1,77 @@
-import uvicorn
-from fastapi import FastAPI  # type:ignore
-from fastapi.responses import UJSONResponse  # type:ignore
-from mabel.logging import get_logger, set_log_name
-from models import ExampleModel  # remove this in your app
+# import uvicorn
+# from fastapi import FastAPI  # type:ignore
+# from fastapi.responses import UJSONResponse  # type:ignore
+# from mabel.logging import get_logger, set_log_name
+from google.cloud import resource_manager
+import sys
+import numpy as np  # am i using this???
+import pandas as pd
+import os
+import subprocess
+import requests
+import json
+import fileinput
 
-set_log_name("TEMPLATE")
-logger = get_logger()
-app = FastAPI()
+#Need to run this for Auth
+#gcloud auth application-default login
 
+client = resource_manager.Client()
+access_token = os.popen('gcloud auth print-access-token').read()[:-1]
 
-@app.post('/example', response_class=UJSONResponse)  # remove this in your app
-def handle_example_request(parameters: ExampleModel):
-    try:
-        # hash the payload so we have a unique ID for it, the allows us to 
-        # match logs
-        request_id = abs(hash(parameters.json()))
-        logger.debug(F"Started ID:{request_id}")
+#Make a list of all the repos
+response = requests.get('https://eu.gcr.io/v2/_catalog', auth=('_token', access_token))
+with open('repo_image_list.json', 'w') as f:
+    print(response.json(), file=f)
 
-        # this is where the work should be done
+#tidy the output so ' becomes " - validating json
+with fileinput.FileInput("repo_image_list.json", inplace=True, backup='.bak') as file:
+    for line in file:
+        print(line.replace('\'', "\""), end='')
+        
+df = pd.read_json ('repo_image_list.json')
 
-    except Exception as err:
-        error_message = F"{type(err).__name__}: {err} (ID:{request_id})"
-        logger.error(error_message)
-        return {}, 500
-    finally:
-        logger.debug(F"Finished ID:{request_id}")
+#Need to run this to get further info
+#gcloud container images describe eu.gcr.io/dcsgva-devsecops-prd/ms-twitter-publisher
 
-    return {}, 200
+with open('repo_image_tags_list.json', 'w') as f:
+    #just to see it running
+    print(response.json(), file=f)
+    
+for index, repositories in df.iterrows():
+    repo = str(repositories['repositories'])
+    resp = requests.get('https://eu.gcr.io/v2/' + repo + '/tags/list', auth=('_token', access_token))
 
+    with open('repo_image_tags_list.json', 'a') as f:
+        print(repo) #just to watch it progressing
+        print(resp.json(), file=f)
+        #resp.json()
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)  
+#df_repo_image_list.to_json("datatester.json", orient = "records", date_format = "epoch", double_precision = 10, force_ascii = True, date_unit = "ms", default_handler = None)
+
+with fileinput.FileInput("repo_image_tags_list.json", inplace=True, backup='.bak') as file:
+    for line in file:
+        print(line.replace('\'', "\""), end='')
+
+df_img_tags_list = pd.read_json ('repo_image_tags_list.json', lines=True)
+
+print(df_img_tags_list)
+
+df_img_tags_list.to_json("datatester.json", orient = "records", date_format = "epoch", double_precision = 10, force_ascii = True, date_unit = "ms", default_handler = None)
+
+df_img_tags_list = df_img_tags_list.iloc[1:]
+
+print(df_img_tags_list)
+
+with open('repo_name_tags_list.csv', 'w') as f:
+    print("repo|tags", file=f)
+
+#for index, repositories in df.iterrows():
+ #   repo = str(repositories['repositories'])
+ #   #print(repo)
+for index, row in df_img_tags_list.iterrows():
+    tags = str(['manifest'])
+    print(index, row['name'], row['tags'], row['manifest']) #just to watch it progressing
+    dft = pd.DataFrame(columns=[index, row['name'], row['tags']])
+    
+    with open('repo_name_tags_list.csv', 'a') as f:
+        print((row['name'], row['tags']), file=f)
